27fb30818eb8214ffebe6fa3391f2e6cfedfc55f
Zhen HUANG <me@zhenhuang.tw>
2025-07-13 21:57:35 +0800

    fix: assign route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,11 +15,11 @@ export default defineNuxtConfig({
         { name: 'description', content: '蒐集台灣各校學生自治組織規章，包含歷史版本。' }
       ],
       link: [
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-        { rel: 'manifest', href: '/site.webmanifest' },
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/tw-student-regulation-archive/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/tw-student-regulation-archive/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/tw-student-regulation-archive/favicon-16x16.png' },
+        { rel: 'manifest', href: '/tw-student-regulation-archive/site.webmanifest' },
+        { rel: 'icon', type: 'image/x-icon', href: '/tw-student-regulation-archive/favicon.ico' }
       ]
     },
     // For GitHub Pages deployment, change to your repo name, e.g., '/student-regulations/'
