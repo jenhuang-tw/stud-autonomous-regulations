3705799f66f61f95595880aaae773676c2222679
Zhen HUANG <me@zhenhuang.tw>
2025-07-13 21:09:12 +0800

    fix: install module nuxt-icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: process.env.NODE_ENV === 'development' },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon'
+  ],
   app: {
     head: {
       title: '臺灣學生自治規章典藏',
